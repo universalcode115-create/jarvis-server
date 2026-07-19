3e26c01a7930d997ccbf890a3f55edda73c836e3
universalcode115-create <universalcode115@gmail.com>
2026-07-19 19:26:46 +0530

    Implement caching functions with expiration

--- core/cache.js
+++ core/cache.js
@@ -1,1 +1,49 @@
+const cache = new Map();
 
+const CACHE_TIME = 5 * 60 * 1000; // 5 minutes
+
+function get(key) {
+
+    const item = cache.get(key);
+
+    if (!item) return null;
+
+    if (Date.now() > item.expire) {
+
+        cache.delete(key);
+
+        return null;
+
+    }
+
+    return item.data;
+
+}
+
+function set(key, data) {
+
+    cache.set(key, {
+
+        data,
+
+        expire: Date.now() + CACHE_TIME
+
+    });
+
+}
+
+function clear() {
+
+    cache.clear();
+
+}
+
+module.exports = {
+
+    get,
+
+    set,
+
+    clear
+
+};

55ae6194997264ee5b693bf1a35f168b8ff26386
universalcode115-create <universalcode115@gmail.com>
2026-07-19 18:38:55 +0530

    Add formatResponse function for multiple providers

--- core/formatter.js
+++ core/formatter.js
@@ -1,1 +1,39 @@
+function formatResponse(provider,data){
 
+let text="";
+
+switch(provider){
+
+case "gemini":
+text=data?.candidates?.[0]?.content?.parts?.[0]?.text||"";
+break;
+
+case "groq":
+text=data?.choices?.[0]?.message?.content||"";
+break;
+
+case "openrouter":
+text=data?.choices?.[0]?.message?.content||"";
+break;
+
+case "huggingface":
+text=data?.[0]?.generated_text||"";
+break;
+
+default:
+text="No response";
+}
+
+return{
+
+success:true,
+
+provider,
+
+text
+
+};
+
+}
+
+module.exports=formatResponse;

ac194855515e5fdf81143d231051fce7023d0e17
universalcode115-create <universalcode115@gmail.com>
2026-07-19 19:12:26 +0530

    Update health.js

--- core/health.js
+++ core/health.js
@@ -1,1 +1,47 @@
+const health = {
 
+    Gemini: true,
+
+    Groq: true,
+
+    OpenRouter: true,
+
+    HuggingFace: true
+
+};
+
+function online(name){
+
+    health[name]=true;
+
+}
+
+function offline(name){
+
+    health[name]=false;
+
+}
+
+function isOnline(name){
+
+    return health[name];
+
+}
+
+function all(){
+
+    return health;
+
+}
+
+module.exports={
+
+online,
+
+offline,
+
+isOnline,
+
+all
+
+};

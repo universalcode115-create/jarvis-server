d1ca0e5e0957b062321169398a587d2385041a26
universalcode115-create <universalcode115@gmail.com>
2026-07-19 18:39:56 +0530

    Implement chat memory storage with save and get functions

--- core/memory.js
+++ core/memory.js
@@ -1,1 +1,35 @@
+const chats={};
 
+function save(id,role,text){
+
+if(!chats[id]) chats[id]=[];
+
+chats[id].push({
+
+role,
+
+content:text
+
+});
+
+if(chats[id].length>20){
+
+chats[id].shift();
+
+}
+
+}
+
+function get(id){
+
+return chats[id]||[];
+
+}
+
+module.exports={
+
+save,
+
+get
+
+};
